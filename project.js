d61903edbb00216cc889827a8b39199d5cb103ab
Sudip Dhua <154453399+Sudip03Dhua@users.noreply.github.com>
2024-04-18 17:47:46 +0000

    "added html files in node js fs "

--- project.js
+++ project.js
@@ -10,15 +10,32 @@ const server = http.createServer((req, res) => {
     //index//
     //about//
     //services//
-
+    res.writeHead(200,{'Content-type':'text/html'})
     const path = req.url
+
     if (path == "/about") {
-        console.log("about page")
+         
+        const fileContent =  fs.readFileSync("./views/about.html")
+        res.write(fileContent);
+        res.end()
+        
+        //console.log("about page")
+
     } else if (path == "/") {
-        console.log("home page")
+
+       
+        const fileContent =  fs.readFileSync("./views/home.html")
+        res.write(fileContent);
+        res.end()
+
+        //console.log("home page")
     }
     else if (path == "/services") {
-        console.log("services page")
+         
+        const fileContent =  fs.readFileSync("./views/services.html")
+        res.write(fileContent);
+        res.end()
+        // console.log("services page")
     }
 
 
